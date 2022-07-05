f61124bba48cf40ff786eac08f3d787991f62a64
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-07-05 16:25:13 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div className>
+
+      <h1>Dope spotify clone</h1>
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+      <div>
+
+        {/* Player */}
+      </div>
+    </div>
+  )
+}
+
+export default Home;
