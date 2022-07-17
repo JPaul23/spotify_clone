51e7cba9e0d075335e6db6e1002544e5f31f8be7
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-07-17 16:10:04 +0200

    next-auth authentication

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,21 @@
-function Login() {
+import { getProviders, signIn } from 'next-auth/react';
+
+function Login({ providers }) {
     return (
         <div>
-           <h1>The login</h1> 
+            <h1>The login</h1>
         </div>
     )
 }
 
 export default Login;
+
+export async function getServerSideProps() {
+    const providers = await getProviders();
+
+    return {
+        props: {
+            providers
+        }
+    }
+}
